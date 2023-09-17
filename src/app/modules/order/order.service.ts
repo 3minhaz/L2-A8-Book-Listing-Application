068b06c93c56d8cc787847db8da8c0692470b5c4
3minhaz <3minhaz@gmail.com>
2023-09-18 00:37:14 +0600

    get single order done

--- src/app/modules/order/order.service.ts
+++ src/app/modules/order/order.service.ts
@@ -1,3 +1,5 @@
+import httpStatus from 'http-status'
+import ApiError from '../../../errors/ApiError'
 import prisma from '../../../shared/prisma'
 import { OrderBooks, createOrderBody } from './order.interface'
 
@@ -31,7 +33,16 @@ const createOrder = async (data: createOrderBody, userId: string) => {
 
 const getAllOrder = async (role: string, userId: string) => {
   if (role === 'admin') {
-    return await prisma.order.findMany({ include: { orderBooks: true } })
+    return await prisma.order.findMany({
+      include: {
+        orderBooks: {
+          select: {
+            bookId: true,
+            quantity: true,
+          },
+        },
+      },
+    })
   } else if (role === 'customer') {
     return await prisma.order.findMany({
       where: {
@@ -48,4 +59,37 @@ const getAllOrder = async (role: string, userId: string) => {
     })
   }
 }
-export const OrderService = { createOrder, getAllOrder }
+
+const getSingleOrder = async (role: string, id: string, params: string) => {
+  if (role === 'admin') {
+    return prisma.order.findUnique({
+      where: { id },
+      include: {
+        orderBooks: {
+          select: {
+            bookId: true,
+            quantity: true,
+          },
+        },
+      },
+    })
+  } else if (role === 'customer') {
+    const getOrder = await prisma.order.findUnique({ where: { id: params } })
+
+    if (getOrder?.userId !== id) {
+      throw new ApiError(httpStatus.BAD_REQUEST, 'Forbidden')
+    }
+    return prisma.order.findUnique({
+      where: { id: params },
+      include: {
+        orderBooks: {
+          select: {
+            bookId: true,
+            quantity: true,
+          },
+        },
+      },
+    })
+  }
+}
+export const OrderService = { createOrder, getAllOrder, getSingleOrder }
