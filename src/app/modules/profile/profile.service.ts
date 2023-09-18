30354d8394ff2615aae223baa3edacfc6da4d2a5
3minhaz <3minhaz@gmail.com>
2023-09-18 09:55:15 +0600

    get my profile done

---
+++ src/app/modules/profile/profile.service.ts
@@ -0,0 +1,12 @@
+import prisma from '../../../shared/prisma'
+import { userReturnFields } from '../user/user.constants'
+
+const getMyProfile = async (id: string) => {
+  const result = await prisma.user.findFirst({
+    where: { id },
+    select: userReturnFields,
+  })
+  return result
+}
+
+export const ProfileService = { getMyProfile }
